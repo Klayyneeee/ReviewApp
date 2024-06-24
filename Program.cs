6ea7e330b91e4485e60b2576d9d2cc1cb5a4f1fa
Dauren <113833968+Klayyneeee@users.noreply.github.com>
2024-06-24 14:01:34 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using ReviewApp;
+using ReviewApp.Domain.Interface;
+using ReviewApp.Infrastructure.Data;
+using ReviewApp.Infrastructure.Repository;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IGoodRepository, GoodRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
